a7df880059b39264ba79673b0d00c6c3e18eba10
Robert Leist <robert.leist@dfki.de>
2025-09-23 12:36:22 +0200

    Improved ModelCache

--- models/model_cache.py
+++ models/model_cache.py
@@ -1,25 +1,27 @@
+import threading
+from collections import OrderedDict
 from models.model_registry import MODEL_REGISTRY
 
-
 class ModelCache:
-    """
-    A simple in-memory cache for storing and retrieving models.
-    """
-    def __init__(self):
-        self.cache = {}
+    def __init__(self, size_limit=3):
+        """
+        Cache to hold a limited number of loaded models. Thread-safe.
+        :param size_limit: Maximum number of models to cache. Default is 3.
+        """
+        self.size_limit = size_limit
+        self.cache = OrderedDict()
+        self.lock = threading.Lock()  # Lock for thread safety
 
     def get(self, model_identifier):
-        """
-        Retrieve a model from the cache by its name.
+        with self.lock:  # Acquire lock
+            if model_identifier in self.cache:
+                model = self.cache.pop(model_identifier)
+                self.cache[model_identifier] = model
+                return model
+            else:
+                model = MODEL_REGISTRY.load_model(model_identifier)
+                if len(self.cache) >= self.size_limit:
+                    self.cache.popitem(last=False)
+                self.cache[model_identifier] = model
+                return model
 
-        Args:
-            model_identifier (str): The name of the model to retrieve.
-        Returns:
-            The model if found, else loads the model into the cache and returns it.
-        """
-        if model_identifier in self.cache:
-            return self.cache[model_identifier]
-        else:
-            model = MODEL_REGISTRY.load_model(model_identifier)
-            self.cache[model_identifier] = model
-            return model
