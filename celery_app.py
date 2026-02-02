0b505a23e9c835864d0f57b1624fe2354d849343
Robert Leist <robert.leist@dfki.de>
2026-02-02 14:53:43 +0100

    added celery

---
+++ celery_app.py
@@ -0,0 +1,8 @@
+from celery import Celery
+from paths import REDIS_URL
+
+celery_app = Celery(
+    "iquana_service_prompted_segmentation", # Must match the name in your other services
+    broker=f"{REDIS_URL}/0",
+    backend=f"{REDIS_URL}/1"
+)
